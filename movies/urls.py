c198bf006370e5a3561debf5cbc1be0fd4083b1a
mehmet <tas349873@gmail.com>
2022-09-27 15:58:07 +0300

    update

--- movies/urls.py
+++ movies/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index,name="home_page"),
-    path("movies", views.movies,name="movie_page"),
+    path("movies", views.movies,name="movies_page"),
     path("movies/<slug:slug>", views.movie_details,name="movie_details")    
 ]
 
--- movies/views.py
+++ movies/views.py
@@ -7,6 +7,7 @@ data = {
             "title": "film adı 1",
             "description": "film açıklama 1",
             "imageUrl": "m1.jpg",
+            "coverImage": "cover1.jpg",
             "slug": "film-adi-1",
             "language": "english",
             "date": date(2021,10,10)
@@ -15,6 +16,7 @@ data = {
             "title": "film adı 2",
             "description": "film açıklama 2",
             "imageUrl": "m2.jpg",
+            "coverImage": "cover2.jpg",
             "slug": "film-adi-2",
             "language": "english",
             "date": date(2021,5,10)
@@ -23,6 +25,7 @@ data = {
             "title": "film adı 3",
             "description": "film açıklama 3",
             "imageUrl": "m3.jpg",
+            "coverImage": "cover3.jpg",
             "slug": "film-adi-3",
             "language": "english",
             "date": date(2021,10,5)
@@ -31,36 +34,36 @@ data = {
             "title": "film adı 4",
             "description": "film açıklama 4",
             "imageUrl": "m4.jpg",
-            "slug": "film-adi-4",
-            "language": "english",
-            "date": date(2020,10,5)
-        },
-        {
-            "title": "film adı 3",
-            "description": "film açıklama 3",
-            "imageUrl": "m3.jpg",
-            "slug": "film-adi-3",
-            "language": "english",
-            "date": date(2021,10,5)
-        },
-        {
-            "title": "film adı 4",
-            "description": "film açıklama 4",
-            "imageUrl": "m4.jpg",
+            "coverImage": "cover1.jpg",
             "slug": "film-adi-4",
             "language": "english",
             "date": date(2020,10,5)
         }
     ],
-    "sliders": []
+    "sliders": [
+        {
+            "slider_image": "slider1.jpg",
+            "url": "film-adi-1"
+        },
+         {
+            "slider_image": "slider2.jpg",
+            "url": "film-adi-2"
+        },
+         {
+            "slider_image": "slider3.jpg",
+            "url": "film-adi-3"
+        }
+    ]
 }
 
 # Create your views here.
 
 def index(request):
     movies = data["movies"][-4:]
+    sliders = data["sliders"]
     return render(request, 'index.html', {
-        "movies": movies
+        "movies": movies,
+        "sliders": sliders
     })
 
 def movies(request):
@@ -70,6 +73,14 @@ def movies(request):
     })
 
 def movie_details(request, slug):
+    movies = data["movies"]
+    # selectedMovie = None
+    # for movie in movies:
+    #     if movie["slug"] == slug:
+    #         selectedMovie = movie
+
+    selectedMovie = next(movie for movie in movies if movie["slug"] == slug)
+
     return render(request, 'movie-details.html', {
-        "slug": slug
+        "movie": selectedMovie
 })
